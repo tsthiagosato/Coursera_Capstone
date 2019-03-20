5f38eac482be6447ca6ae4854a27340491ce5cac
tsthiagosato <ts.thiagosato@gmail.com>
2019-03-20 19:42:36 -0300

    Add files via upload

---
+++ Capstone W1.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Coursera_Capstone week 1 assignment 
+# 
+# This notebook was built for the week 1 assignment of the Capstone Project 
+# 
+# 
+
+# In[4]:
+
+
+# Importing the necessary libraries
+import pandas as pd
+import numpy as np
+
+
+# In[5]:
+
+
+# Printing the hello capstone
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
